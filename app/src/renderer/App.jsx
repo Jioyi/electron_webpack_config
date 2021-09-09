b039d2b463c084e21596492c4ca5a6d829c766a7
Carlos Alberto <karlosagreda@hotmail.com>
2021-09-09 01:19:51 -0300

    add

---
+++ app/src/renderer/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = () => {
+  console.log("ño3",process.env.PORT);
+  return (
+    <div><h1>electron-react-boilerplate</h1></div>
+  );
+}
+export default App;
